27d69ab230c6c110bede90d1fa1551aa2e19380a
Mateus Abelli <mateusabelli@gmail.com>
2022-11-26 12:18:58 -0300

    Add Pusher

---
+++ lib/pusher.ts
@@ -0,0 +1,9 @@
+import Pusher from "pusher";
+
+export const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID as string,
+  key: process.env.PUSHER_KEY as string,
+  secret: process.env.PUSHER_SECRET as string,
+  cluster: process.env.PUSHER_CLUSTER as string,
+  useTLS: true,
+});
---
+++ pages/api/message.ts
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { pusher } from "../../lib/pusher";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Invalid method" });
+  }
+
+  try {
+    const { username, message } = req.body;
+
+    pusher.trigger("chat", "message", {
+      id: Math.floor(Math.random() * 1000),
+      message,
+      username,
+    });
+
+    res.status(200).json({ message: "Message sent" });
+  } catch (error) {
+    console.error("api/messages error", error);
+    res.status(500);
+  }
+}
