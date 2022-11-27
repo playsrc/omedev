cc57fba0ff75b739a223c43b634f6b8d24601887
Mateus Abelli <mateusabelli@gmail.com>
2022-11-26 21:56:47 -0300

    Add Chakra UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
