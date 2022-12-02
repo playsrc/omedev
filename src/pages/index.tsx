121d90a69a5f554adb0d8610c6be28c769b118aa
Mateus Abelli <mateusabelli@gmail.com>
2022-12-02 20:07:07 -0300

    Add meta tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,6 +42,10 @@ export default function Home() {
       <Head>
         <title>OmeDEV: Talk to developers!</title>
         <link rel="icon" href="/icon.png" type="image/png" />
+        <meta
+          name="description"
+          content="The Internet is full of cool developers; OmeDEV lets you meet them. When you use OmeDEV, we pick someone else at random so you can have a one-on-one chat."
+        />
       </Head>
       <Grid
         templateRows="max-content 1fr"
